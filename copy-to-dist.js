99f5fec7fc5c51dd7be42b5c5a97f1abdd15c28d
Miroslav Kozel <mirek.kozel@gmail.com>
2020-04-20 17:48:37 +0200

    move to new dev environment

--- copy-to-dist.js
+++ copy-to-dist.js
@@ -1,6 +1,6 @@
 const APP_NAME = require('./package.json').name;
 
 const fs = require('fs-extra');
-fs.copySync(`./build/${APP_NAME}.js`,`../../chameleon-backend/www/static/${APP_NAME}.js`);
-fs.copySync(`./build/${APP_NAME}.html`,`../../chameleon-backend/www/html/${APP_NAME}.html`);
-fs.copySync(`./build/${APP_NAME}.css`,`../../chameleon-backend/www/static/${APP_NAME}.css`);
+fs.copySync(`./build/${APP_NAME}.js`,`../chameleon-backend/www/static/${APP_NAME}.js`);
+fs.copySync(`./build/${APP_NAME}.html`,`../chameleon-backend/www/html/${APP_NAME}.html`);
+fs.copySync(`./build/${APP_NAME}.css`,`../chameleon-backend/www/static/${APP_NAME}.css`);
